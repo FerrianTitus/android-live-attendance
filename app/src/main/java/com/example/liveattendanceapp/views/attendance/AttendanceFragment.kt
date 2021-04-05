48c6ebab8b24ba9c8c6bfde2e0305cd750f6ea76
FerrianTitus <ferrian00@gmail.com>
2021-04-05 17:47:28 +0700

    check permissions maps

--- app/src/main/java/com/example/liveattendanceapp/views/attendance/AttendanceFragment.kt
+++ app/src/main/java/com/example/liveattendanceapp/views/attendance/AttendanceFragment.kt
@@ -1,20 +1,34 @@
 package com.example.liveattendanceapp.views.attendance
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.example.liveattendanceapp.R
 import com.example.liveattendanceapp.databinding.FragmentAttendanceBinding
+import com.example.liveattendanceapp.dialog.MyDialog
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.StringBuilder
 
 class AttendanceFragment : Fragment(), OnMapReadyCallback {
+
+    companion object{
+        private const val REQUEST_CODE_MAP_PERMISSIONS = 1000
+    }
+
+    private val mapPermissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     private var mapAttendance: SupportMapFragment? = null
     private var map: GoogleMap? = null
@@ -38,6 +52,35 @@ class AttendanceFragment : Fragment(), OnMapReadyCallback {
         setupMaps()
     }
 
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_CODE_MAP_PERMISSIONS -> {
+                var isHasPermission = false
+                val permissionNotGranted = StringBuilder()
+
+                for (i in permissions.indices){
+                    isHasPermission = grantResults[i] == PackageManager.PERMISSION_GRANTED
+
+                    if (!isHasPermission){
+                        permissionNotGranted.append("${permissions[i]}\n")
+                    }
+                }
+
+                if (isHasPermission) {
+                    setupMaps()
+                }else{
+                    val message = permissionNotGranted.toString() + "\n" + getString(R.string.not_granted)
+                    MyDialog.dynamicDialog(context, getString(R.string.required_permission), message)
+                }
+            }
+        }
+    }
+
     private fun setupMaps() {
         mapAttendance = childFragmentManager.findFragmentById(R.id.map_attendance) as SupportMapFragment
         mapAttendance?.getMapAsync(this)
@@ -45,14 +88,33 @@ class AttendanceFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap?) {
         map = googleMap
-        val gunadarma = LatLng(-6.367963791499807, 106.83322406903427)
-        map?.addMarker(
-                MarkerOptions()
-                        .position(gunadarma)
-                        .title("Marker in Gunadarma")
-        )
-        map?.moveCamera(CameraUpdateFactory.newLatLng(gunadarma))
-        map?.animateCamera(CameraUpdateFactory.zoomTo(20f))
+        if (checkPermission()) {
+            val gunadarma = LatLng(-6.367963791499807, 106.83322406903427)
+            map?.moveCamera(CameraUpdateFactory.newLatLng(gunadarma))
+            map?.animateCamera(CameraUpdateFactory.zoomTo(20f))
+
+            goToCurrentLocation()
+        }else{
+            setRequestPermission()
+        }
+    }
+
+    private fun setRequestPermission() {
+        requestPermissions(mapPermissions, REQUEST_CODE_MAP_PERMISSIONS)
+    }
+
+    private fun goToCurrentLocation() {
+
+    }
+
+    private fun checkPermission(): Boolean {
+        var isHasPermission = false
+        context?.let {
+            for (permission in mapPermissions){
+                isHasPermission = ActivityCompat.checkSelfPermission(it, permission) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+        return isHasPermission
     }
 
 }
